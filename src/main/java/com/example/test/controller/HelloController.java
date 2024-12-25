e656799a31567fbbd449d08070d21217ae16f45d
DoNgocDong <ngocdong2110.2003@gmail.com>
2024-12-25 14:35:33 +0700

    apply i18n for controller

--- src/main/java/com/example/test/controller/HelloController.java
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -1,13 +1,33 @@
 package com.example.test.controller;
 
+import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Locale;
 
 @RestController
+@RequestMapping(value = "/", consumes = MediaType.ALL_VALUE)
 public class HelloController {
+    private final MessageSource messageSource;
 
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello World";
+    public HelloController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> sayHello(
+        @RequestParam(name = "name", defaultValue = "World") String name,
+        WebRequest request
+    ){
+        Locale locale = request.getLocale();
+        final String res =  messageSource.getMessage("greeting", new Object[]{name}, locale);
+
+        return ResponseEntity.ok(res);
     }
 }
